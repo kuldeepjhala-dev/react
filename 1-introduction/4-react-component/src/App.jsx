ff966ec534dc3e17264b520919cf4a2321ec4d93
kuldeepjhala-dev <kuldeephjhala@gmail.com>
2024-01-13 23:07:43 +0530

    Applying css styles by importing css file

--- 1-introduction/4-react-component/src/App.jsx
+++ 1-introduction/4-react-component/src/App.jsx
@@ -1,13 +1,10 @@
-//NO need to import React and React-dom files
-import Hello from "./components/Hello";
-import { Hello2 } from "./components/Hello";
 import Greeting from "./components/Greeting";
 
 function App() {
   return (
     <>
-      <Hello />
-      <Hello2 />
+      {/* <Hello />
+      <Hello2 /> */}
       <Greeting />
     </>
   );
--- 1-introduction/4-react-component/src/components/Greeting.jsx
+++ 1-introduction/4-react-component/src/components/Greeting.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Greeting.css"; //css file import
 
 function Greeting() {
+  const FirstName = "Kuldeepsinh";
+
+  const firstName = (name) => name.toUpperCase();
+
   return (
-    <div>
-      Greeting
+    <div className="bg-dark">
+      {/*use className instead of class*/}
+      <h1>Hello {firstName(FirstName)}</h1> {/*if you want to use javascript*/}
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus
+        architecto tempora quae voluptate consequuntur necessitatibus
+        perspiciatis doloremque ut id voluptatum!
+      </p>
+      <h2>Form</h2>
+      <div>
+        <label htmlFor="fName">First Name</label>
+        {/*use htmlFor instead of html*/}
+        <input type="text" id="fName" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Greeting
+export default Greeting;
