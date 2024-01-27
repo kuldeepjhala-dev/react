8a2d42819c2037d60bd8bf0e17c96705051693b5
kuldeepjhala-dev <kuldeephjhala@gmail.com>
2024-01-27 18:38:37 +0530

    handling multiple input of form using one state

---
+++ 6-more-about-forms/2-handel-form-input-using-single-function/src/App.js
@@ -0,0 +1,10 @@
+import HandelMultipleInput from './components/HandelMultipleInput';
+function App() {
+  return (
+    <div className="App">
+      <HandelMultipleInput />
+    </div>
+  );
+}
+
+export default App;
